1c37110b0ef63fb45c0a1656e0d544f541cddcd3
dawven <15927122432@163.com>
2017-12-24 10:02:27 +0800

    goods页面

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-header></v-header>
+    <v-header :seller="seller"></v-header>
     <div class="tab border-1px">
       <div class="tab-item">
         <router-link to="/goods">商品</router-link>
@@ -19,11 +19,20 @@
 <script>
   import header from 'components/header/header';
 
+  const ERR_OK = 0;
   export default {
     data() {
       return {
         seller: {}
       };
+    },
+    created() {
+      this.$http.get('/api/seller').then((response) => {
+        response = response.body;
+        if (response.errno === ERR_OK) {
+          this.seller = response.data;
+        }
+      });
     },
     components: {
       'v-header': header
